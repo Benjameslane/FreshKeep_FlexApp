18563e389a98f92ecacb403fee166089549d1dfd
Benjameslane <ben.james.lane9>
2023-09-29 09:59:46 -0600

    updated SQL database and added User Model

---
+++ FullStackAuth_WebAPI/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FullStackAuth_WebAPI.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public UserType Type { get; set; } // Enum for UserType (Regular/Homeowner or StoreOwner)
+        // ... additional properties if needed
+    }
+
+    public enum UserType
+    {
+        Homeowner,
+        StoreOwner
+        
+    }
+}

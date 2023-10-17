68858f81bc5ba30152fe0c9a4f664a1944605d62
Benjameslane <ben.james.lane9>
2023-10-17 13:02:16 -0600

    migration added to repo eval

--- FullStackAuth_WebAPI/DataTransferObjects/DiscountedItemDto.cs
+++ FullStackAuth_WebAPI/DataTransferObjects/DiscountedItemDto.cs
@@ -1,1 +1,15 @@
-﻿
+﻿namespace FullStackAuth_WebAPI.DataTransferObjects
+{
+    public class DiscountedItemDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public double OriginalPrice { get; set; }
+        public double DiscountedPrice { get; set; }
+        public double DiscountAmount { get; set; }
+        public string StoreOwnerId { get; set; }
+
+        // Additional properties if needed.
+    }
+}
